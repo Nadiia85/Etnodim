f70c2a3d4fcb266bdf755ac3d95c16146922028a
Nadiia85 <127414436+Nadiia85@users.noreply.github.com>
2023-07-24 18:58:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function  toggleMenu() {
+  const unfoldedMenu = document.getElementById('menu');
+  unfoldedMenu.classList.toggle("menu-open");
+
+}
+function  toggleFilter() {
+  const unfoldedMenu = document.getElementById('filter__block');
+  unfoldedMenu.classList.toggle("filter__block-open");
+
+}
+
+
+
+
+
